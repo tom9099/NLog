d436eb3f8e82ba0389a37ebfd1ba73f396c71725
tomi <tamaskrizsanyik@gmail.com>
2017-02-12 12:37:52 +0000

    Boost our priority

--- testclient/main.cpp
+++ testclient/main.cpp
@@ -1,8 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <Windows.h>
 
 int main(int argc, char *argv[])
 {
+    SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
